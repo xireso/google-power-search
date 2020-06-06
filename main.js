a0a7c98771d5d4897f58ccbfb7122498d461cd2b
Steven Xu <weareallhalfminotaur@gmail.com>
2020-06-06 00:57:03 -0500

    button hover / toggle mechhanics
    Co-Authored-By: Joshua Lin <jlin-orange@users.noreply.github.com>

--- main.js
+++ main.js
@@ -39,9 +39,26 @@ function searchGoogle() {
 console.log(document.getElementsByClassName('exact'));
 
 // Select Any / All for text in Title
-function intitleAnySelector() {}
+function intitleAnySelector() {
+	var any = document.getElementById("intitle-any");
+	any.classList.remove("button");
+	any.classList.add("button-highlight");
 
-function intitleAllSelector() {}
+	var all = document.getElementById("intitle-all");
+	all.classList.remove("button-highlight");
+	all.classList.add("button");
+
+}
+
+function intitleAllSelector() {
+	var all = document.getElementById("intitle-all");
+	all.classList.remove("button");
+	all.classList.add("button-highlight");
+	
+	var any = document.getElementById("intitle-any");
+	any.classList.remove("button-highlight");
+	any.classList.add("button");
+}
 
 /**var input = '';
 	input = input + document.getElementsByClassName('exact')[1].value;
@@ -50,30 +67,167 @@ function intitleAllSelector() {}
 **/
 
 // Select Any / All for text in URL
-function inurlAnySelector() {}
+function inurlAnySelector() {
+	var any = document.getElementById("inurl-any");
+	any.classList.remove("button");
+	any.classList.add("button-highlight");
 
-function inurlAllSelector() {}
+	var all = document.getElementById("inurl-all");
+	all.classList.remove("button-highlight");
+	all.classList.add("button");
+	
+}
+
+function inurlAllSelector() {
+	var all = document.getElementById("inurl-all");
+	all.classList.remove("button");
+	all.classList.add("button-highlight");
+
+	var any = document.getElementById("inurl-any");
+	any.classList.remove("button-highlight");
+	any.classList.add("button");
+
+}
 
 // Select Any / All for text in Text
-function intextAnySelector() {}
+function intextAnySelector() {
+	var any = document.getElementById("intext-any");
+	any.classList.remove("button");
+	any.classList.add("button-highlight");
 
-function intextAllSelector() {}
+	var all = document.getElementById("intext-all");
+	all.classList.remove("button-highlight");
+	all.classList.add("button");
+}
+
+function intextAllSelector() {
+	var all = document.getElementById("intext-all");
+	all.classList.remove("button");
+	all.classList.add("button-highlight");
+
+	var any = document.getElementById("intext-any");
+	any.classList.remove("button-highlight");
+	any.classList.add("button");
+}
 
 // Select Any / All for text in Links
-function inlinksAnySelector() {}
 
-function inlinksAllSelector() {}
+function inlinksAnySelector() {
+	var any = document.getElementById("inlinks-any");
+	any.classList.remove("button");
+	any.classList.add("button-highlight");
+
+	var all = document.getElementById("inlinks-all");
+	all.classList.remove("button-highlight");
+	all.classList.add("button");
+}
+
+function inlinksAllSelector() {
+	var all = document.getElementById("inlinks-all");
+	all.classList.remove("button");
+	all.classList.add("button-highlight");
+
+	var any = document.getElementById("inlinks-any");
+	any.classList.remove("button-highlight");
+	any.classList.add("button");
+}
 
 
 // file type selector
-function pdfSpecificSelector() {}
-function txtSpecificSelector() {}
-function rtfSpecificSelector() {}
-function svgSpecificSelector() {}
-function texSpecificSelector() {}
-function docSpecificSelector() {}
-function pptSpecificSelector() {}
-function xlsSpecificSelector() {}
+function pdfSpecificSelector() {
+	var filetype = document.getElementById("pdf-file");
+	if (filetype.classList.contains("button-highlight")) {
+		filetype.classList.remove("button-highlight");
+		filetype.classList.add("button");
+	}
+	else {
+		filetype.classList.remove("button");
+		filetype.classList.add("button-highlight");
+	}
+}
+
+function txtSpecificSelector() {
+	var filetype = document.getElementById("txt-file");
+	if (filetype.classList.contains("button-highlight")) {
+		filetype.classList.remove("button-highlight");
+		filetype.classList.add("button");
+	}
+	else {
+		filetype.classList.remove("button");
+		filetype.classList.add("button-highlight");
+	}
+}
+function rtfSpecificSelector() {
+	var filetype = document.getElementById("rtf-file");
+	if (filetype.classList.contains("button-highlight")) {
+		filetype.classList.remove("button-highlight");
+		filetype.classList.add("button");
+	}
+	else {
+		filetype.classList.remove("button");
+		filetype.classList.add("button-highlight");
+	}
+}
+
+function svgSpecificSelector() {
+	var filetype = document.getElementById("svg-file");
+	if (filetype.classList.contains("button-highlight")) {
+		filetype.classList.remove("button-highlight");
+		filetype.classList.add("button");
+	}
+	else {
+		filetype.classList.remove("button");
+		filetype.classList.add("button-highlight");
+	}
+}
+
+function texSpecificSelector() {
+	var filetype = document.getElementById("tex-file");
+	if (filetype.classList.contains("button-highlight")) {
+		filetype.classList.remove("button-highlight");
+		filetype.classList.add("button");
+	}
+	else {
+		filetype.classList.remove("button");
+		filetype.classList.add("button-highlight");
+	}
+}
+
+function docSpecificSelector() {
+	var filetype = document.getElementById("doc-file");
+	if (filetype.classList.contains("button-highlight")) {
+		filetype.classList.remove("button-highlight");
+		filetype.classList.add("button");
+	}
+	else {
+		filetype.classList.remove("button");
+		filetype.classList.add("button-highlight");
+	}
+}
+
+function pptSpecificSelector() {
+	var filetype = document.getElementById("ppt-file");
+	if (filetype.classList.contains("button-highlight")) {
+		filetype.classList.remove("button-highlight");
+		filetype.classList.add("button");
+	}
+	else {
+		filetype.classList.remove("button");
+		filetype.classList.add("button-highlight");
+	}
+}
+
+function xlsSpecificSelector() {
+	var filetype = document.getElementById("xls-file");
+	if (filetype.classList.contains("button-highlight")) {
+		filetype.classList.remove("button-highlight");
+		filetype.classList.add("button");
+	}
+	else {
+		filetype.classList.remove("button");
+		filetype.classList.add("button-highlight");
+	}
+}
 
 
 // file type exclusions
