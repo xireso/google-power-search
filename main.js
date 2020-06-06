1bb8b731861f779e0d5ea6cfab5a27b7f6525df6
xireso <rose.xi.611@gmail.com>
2020-06-06 10:29:17 +0900

    added sample input

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 // alert('yessss this works!!! much happy');
+
+URL = 'https://google.com/search?q=where+have+all+the+flowers+gone';
+
+function searchGoogle() {
+	window.open(URL);
+}
+
+console.log(document.getElementsByClassName('exact'));
